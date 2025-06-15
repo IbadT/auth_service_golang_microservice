9aa5bd0d0a54e1d3ff43a056a49cfd4c1f4c51cf
Eduard Kononovich <101503726+IbadT@users.noreply.github.com>
2025-06-15 13:42:24 +0300

    added reposotory.go

--- internal/auth/repository.go
+++ internal/auth/repository.go
@@ -1,1 +1,29 @@
 package auth
+
+import "gorm.io/gorm"
+
+type Repository interface {
+	Login() error
+	Register() error
+	RefreshToken() error
+}
+
+type repository struct {
+	DB *gorm.DB
+}
+
+func NewRepository(db *gorm.DB) Repository {
+	return &repository{DB: db}
+}
+
+func (r *repository) Login() error {
+	return nil
+}
+
+func (r *repository) Register() error {
+	return nil
+}
+
+func (r *repository) RefreshToken() error {
+	return nil
+}
