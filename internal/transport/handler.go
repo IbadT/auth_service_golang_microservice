09096d1e88485d25cb1b3e8942c9f05edfe1cc8f
Eduard Kononovich <101503726+IbadT@users.noreply.github.com>
2025-06-15 13:55:53 +0300

    added handler.go

--- internal/transport/handler.go
+++ internal/transport/handler.go
@@ -1,1 +1,29 @@
 package transportgrpc
+
+import (
+	"context"
+
+	"github.com/IbadT/auth_service_golang_microservice.git/internal/auth"
+	authpb "github.com/IbadT/project-protos/proto/auth"
+)
+
+type Handler struct {
+	svc auth.Service
+	authpb.UnimplementedAuthServiceServer
+}
+
+func NewHandler(s auth.Service) *Handler {
+	return &Handler{svc: s}
+}
+
+func (h *Handler) Login(ctx context.Context, req *authpb.LoginRequest) (*authpb.LoginResponse, error) {
+	return &authpb.LoginResponse{}, nil
+}
+
+func (h *Handler) Register(ctx context.Context, req *authpb.RegisterRequest) (*authpb.RegisterResponse, error) {
+	return &authpb.RegisterResponse{}, nil
+}
+
+func (h *Handler) RefreshToken(ctx context.Context, req *authpb.RefreshTokenResponse) (*authpb.RefreshTokenResponse, error) {
+	return &authpb.RefreshTokenResponse{}, nil
+}
