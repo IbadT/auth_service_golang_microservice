85ed31c1d157bf0f2cca064038dffcea06a79506
Eduard Kononovich <101503726+IbadT@users.noreply.github.com>
2025-06-15 13:46:00 +0300

    added service.go

--- internal/auth/service.go
+++ internal/auth/service.go
@@ -1,1 +1,27 @@
 package auth
+
+type Service interface {
+	Login() error
+	Register() error
+	RefreshToken() error
+}
+
+type service struct {
+	repo Repository
+}
+
+func NewService(r Repository) Service {
+	return &service{repo: r}
+}
+
+func (s *service) Login() error {
+	return s.Login()
+}
+
+func (s *service) Register() error {
+	return s.Register()
+}
+
+func (s *service) RefreshToken() error {
+	return s.RefreshToken()
+}
