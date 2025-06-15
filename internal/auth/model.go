4bd00a280eb4788be85bf708172981cef697ff86
Eduard Kononovich <101503726+IbadT@users.noreply.github.com>
2025-06-15 14:35:18 +0300

    added model.go

--- internal/auth/model.go
+++ internal/auth/model.go
@@ -1,1 +1,9 @@
 package auth
+
+import "github.com/google/uuid"
+
+type User struct {
+	ID       uuid.UUID `json:"id" gorm:"type:uuid;default:gen_random_uuid();primaryKey"`
+	Email    string    `json:"email" gorm:"type:varchar(255);not null;unique"`
+	Password string    `json:"password" gorm:"type:varchar(255);not null"`
+}
